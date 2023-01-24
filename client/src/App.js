752b1e9cefa6ccba963b7ef1013529abc24670e1
abhimran <abhimrann@gmail.com>
2023-01-25 03:05:40 +0600

    Created private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,35 @@
 import './App.scss';
-import { BrowserRouter } from 'react-router-dom';
-import AllRoutes from './config/AllRoutes';
+import { Navigate, Routes, Route } from 'react-router-dom';
+import UserList from './pages/UserList';
+import Login from './pages/Login';
+import Cookies from 'js-cookie';
 
-function App() {
+const ProtectedRoute = ({ isAllowed, redirectPath = '/', children }) => {
+  if (!isAllowed) {
+    return <Navigate to={redirectPath} replace />;
+  }
+
+  return children ? children : <Login />;
+};
+
+const App = () => {
   return (
-    <BrowserRouter>
-      <AllRoutes />
-    </BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login />} />
+
+      <Route
+        path='users'
+        element={
+          <ProtectedRoute
+            redirectPath='/'
+            isAllowed={!!Cookies.get('accessToken')}
+          >
+            <UserList />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   );
-}
+};
 
 export default App;
