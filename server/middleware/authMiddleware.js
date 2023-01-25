e6a5784a5eaa45ba42630c9b066e85272b32125a
abhimran <abhimrann@gmail.com>
2023-01-25 23:37:34 +0600

    added error handlr

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,47 +1,49 @@
-import jwt from "jsonwebtoken";
-import asyncHandler from "express-async-handler";
+import jwt from 'jsonwebtoken';
 
-import { getUserByUserId } from "../database.js";
+import { CustomError } from '../utils/error.js';
+import { getUserByUserId } from '../database.js';
 
 const protect = async (req, res, next) => {
   let token;
 
-  console.log('header ==>', req.headers)
+  console.log('header ==>', req.headers);
 
   if (
     req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
+    req.headers.authorization.startsWith('Bearer')
   ) {
     try {
-      token = req.headers.authorization.split(" ")[1];
+      token = req.headers.authorization.split(' ')[1];
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       const userInfo = await getUserByUserId(decoded.id);
-      delete userInfo.password;
+      delete userInfo?.password;
 
-      req.user = userInfo
+      req.user = userInfo;
 
       next();
     } catch (error) {
       console.error(error);
-      res.status(401);
-      throw new Error("Not authorized, token failed");
+      throw new CustomError(400, 'Server error occured');
     }
   }
 
   if (!token) {
-    res.status(401);
-    throw new Error("Not authorized, no token");
+    throw new CustomError(400, 'Not authorized, no token');
   }
 };
 
 const admin = (req, res, next) => {
-  if (req.user && req.user.isAdmin) {
-    next();
-  } else {
-    res.status(401);
-    throw new Error("Not authorized as an admin");
+  try {
+    if (req.user && req.user.isAdmin) {
+      next();
+    } else {
+      throw new CustomError(400, 'Not authorized as an admin');
+    }
+  } catch (error) {
+    console.log('error ===> ', error);
+    throw new CustomError(500, 'server error occured');
   }
 };
 
